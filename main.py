1cf7727b78e38937537976ef03bf9d97e1ec3f9e
dimosikrus <30956599+dimosikrus@users.noreply.github.com>
2023-12-15 10:09:23 +0500

    Create main.py

---
+++ main.py
@@ -0,0 +1,38 @@
+import telebot
+import re
+from telebot import types
+
+token = '6911214567:AAF5mK6jOADeLzbyYFxt8gKdScESeLuvy5Q'
+bot = telebot.TeleBot(token)
+
+@bot.inline_handler(func=lambda query: len(query.query) > 0)
+def query_text(query):
+    try:
+        r = types.InlineQueryResultArticle(
+                id='1',
+                title="Абоба \"KitoraBot\"",
+                description=query.query,
+                input_message_content=types.InputTextMessageContent(
+                message_text=query.query)
+        )
+        bot.answer_inline_query(query.id, [r])
+    except Exception as e:
+        print(e)
+    bot.answer_inline_query(query.id, r)
+
+@bot.inline_handler(func=lambda query: len(query.query) == 0)
+def empty_query(query):
+    hint = "Введите Chtoto i idite nahyi"
+    try:
+        r = types.InlineQueryResultArticle(
+                id='1',
+                title="Бот \"KitoraBot\"",
+                description=hint,
+                input_message_content=types.InputTextMessageContent(
+                message_text="Yeueueueududueueydydysye")
+        )
+        bot.answer_inline_query(query.id, [r])
+    except Exception as e:
+        print(e)
+
+bot.infinity_polling()
